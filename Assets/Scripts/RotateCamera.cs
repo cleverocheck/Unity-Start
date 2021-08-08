0c9cf4793beb78c8f04f67b3242074b82bd4b00e
Blue Dog <61243749+Blue-Dog-1@users.noreply.github.com>
2021-08-08 17:39:18 +0600

    Update RotateCamera.cs
    оптимизирован метод Updare. часты вызов GetConponent

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour {
     [SerializeField] private float speed = 5;
-    [SerializeField] private GameObject controller;
+    //[SerializeField] private GameObject controller; — не обезатьльно брать GameObject Unity пожет примимать в инсепторе любой компонент.
+    [SerializeField] private Controller controller; 
+    
+    private void Start(){
+        controller = controller.GetComponent<Controller>();
+    }
     private void Update() {
-        if (!controller.GetComponent<Controller>().game_over) {
+        // GetComponent в update это вообще прохо. за такое по рукам бют 
+        // if (!controller.GetComponent<Controller>().game_over) {
+        if (!controller.game_over) {
             GetComponent<Transform>().Rotate(0, speed * Time.deltaTime, 0);
         }
     }
