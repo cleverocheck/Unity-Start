93b55f4c3fb0c1d0236e8120cc6a3f99f69849f8
Blue Dog <61243749+Blue-Dog-1@users.noreply.github.com>
2021-08-08 18:23:52 +0600

    Update CameraShake.cs
    рефакторинг

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -4,14 +4,16 @@ public class CameraShake : MonoBehaviour {
     [SerializeField] private float shake_duraction = 1, factor = 1.2f;
     private Vector3 base_position;
     private void Start() {
-        base_position = GetComponent<Transform>().localPosition;
+        //base_position = GetComponent<Transform>().localPosition;
+        base_position = transform.localPosition;
     }
     private void Update() {
         if (shake_duraction > 0) {
-            GetComponent<Transform>().localPosition = base_position + Random.insideUnitSphere * shake_duraction;
+            transform.localPosition = base_position + Random.insideUnitSphere * shake_duraction;
             shake_duraction -= Time.deltaTime * factor;
         } else {
-            Destroy(GetComponent<CameraShake>());
+            //Destroy(GetComponent<CameraShake>());
+            Destroy(this);
         }
     }
 }
