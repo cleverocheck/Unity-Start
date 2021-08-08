6647146d1aecbe2f97c20ed3819a5e18576bacf7
Blue Dog <61243749+Blue-Dog-1@users.noreply.github.com>
2021-08-08 18:05:04 +0600

    Update StoreController.cs
    оптимизация поиска на сцене

--- Assets/Scripts/StoreController.cs
+++ Assets/Scripts/StoreController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System;
-
+// много харкода
 public class StoreController : MonoBehaviour {
     [SerializeField] private GameObject cubes_container, canvas, cube_text;
     [SerializeField] private Material disabled_material;
@@ -8,8 +8,15 @@ public class StoreController : MonoBehaviour {
     private int[] cubes_price;
     private readonly float cube_spacing_hor = 1.85f, cube_spacing_vert = 2.5f, text_spacing_hor = 260, text_spacing_vert = 330;
     private void Start() {
-        cubes = GameObject.Find("GlobalController").GetComponent<GlobalController>().cubes;
-        cubes_price = GameObject.Find("GlobalController").GetComponent<GlobalController>().cubes_price;
+        // строковые литерылы это зло. исползуй тип а не название типа. плюс в том что не нужно импользовать GetComponent
+        //  cubes = GameObject.Find("GlobalController").GetComponent<GlobalController>().cubes;
+        cubes = FindObjectOfType<GlobalController>().cubes; 
+        cubes_price = FindObjectOfType<GlobalController>().cubes_price;
+        // два раза искать на сене обьект да и ище в одном месте. перемеая удалится как только завершится метод Start
+        var globalController = FindObjectOfType<GlobalController>(); // так лучше. но тогда поля cubes и cubes_price не нужны 
+        cubes = globalController.cubes; 
+        cubes_price = globalController.cubes_price;
+        
         for (int i = 0; i < cubes.Length; i++) {
             GameObject new_cube = Instantiate(cubes[i], cubes_container.transform.position, Quaternion.Euler(-20, 45, -20));
             if (PlayerPrefs.GetInt("record") < cubes_price[i]) new_cube.GetComponent<MeshRenderer>().material = disabled_material;
@@ -18,7 +25,7 @@ public class StoreController : MonoBehaviour {
             if (cubes_price[i] == 0) new_text.GetComponent<TMPro.TextMeshProUGUI>().text = "<color=#e06055>FREE</color>";
             else new_text.GetComponent<TMPro.TextMeshProUGUI>().text = "<color=#e06055>BEST: </color><u color=#e06055>" + cubes_price[i];
             new_text.transform.SetParent(canvas.transform);
-            new_text.transform.localScale = new Vector3(1, 1, 1);
+            new_text.transform.localScale = Vector3.one; // new Vector3(1, 1, 1);
             Vector3 cube_position = new_cube.transform.position, text_position = new_text.transform.position;
             if (i != 9 && i % 3 == 0) {
                 cube_position.x = -cube_spacing_hor;
